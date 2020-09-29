114124d91a6b62e78cccd6659cfffdb3a1c0b010
gamefreakoneone <amogh@outlook.com>
2020-09-29 09:17:49 +0530

    Upload

--- Linked/Doubly.c
+++ Linked/Doubly.c
@@ -26,11 +26,21 @@ Node *create(Node *Start){
             }
             prevnode->next=newnode;
             prevnode=newnode;
-            
         }
         printf("Quit:1");
         scanf("%d",&c);
     }
+    return Start;
+}
+
+Node *insert_beg(Node *Start){
+    Node *newnode;
+    newnode=(struct node *)malloc(sizeof(struct node));
+    printf("Give me the value to insert in the beginning:\n");
+    scanf("%d",&newnode->info);
+    newnode->next=Start;
+    newnode->prev=NULL;
+    Start=newnode;
     return Start;
 }
 
@@ -43,12 +53,43 @@ void display(Node *Start){
     
 }
 
-Node * insert_between(){
-    
+Node * insert_between(Node *Start){
+    int position,i=1;
+    Node *newnode, *previous,*after;
+    newnode=(struct node *)malloc(sizeof(struct node));
+    printf("After how many positions after: \n");
+    scanf("%d",&position);
+    printf("What do you want to store:\n");
+    scanf("%d",&newnode->info);
+    after=Start;
+    previous=Start;
+    while (i<position)
+    {   
+        if(i==1){
+            printf("Continue:");
+            continue;
+        }else
+        {
+            previous=previous->next;
+        }
+        
+        after=after->next;
+        i++;
+    }
+    newnode->next=after;
+    after->prev=newnode;
+    previous->next=newnode;
+    newnode->prev=previous;
+
+    return Start;
 }
 
 int main(){
     Node *Start;
     Start=create(Start);
+    display(Start);
+    Start=insert_beg(Start);
+    display(Start);
+    Start=insert_between(Start);
     display(Start);
 }
