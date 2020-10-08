bb6ae00e34e8edcfc41b0e85c7bbf6aca5d38066
gamefreakoneone <amogh@outlook.com>
2020-10-08 09:50:51 +0530

    Upload

--- dump.c
+++ dump.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 int main()
-{int *ptr;
-int arr[2][3] ={9,12,13,22,30,24};
-ptr=arr;
-*ptr = *ptr + 4;
-printf("
-output is %u",*ptr);
+{
+    char c = 'A';
+    printf("%d , %c",c,c);
 }
