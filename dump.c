7136fa80301816567f56826fc6eae25995c5638a
gamefreakoneone <amogh@outlook.com>
2020-11-26 11:54:04 +0530

    Upload

--- dump.c
+++ dump.c
@@ -1,165 +1,195 @@
-// C++ program for addition of two polynomials 
-// using Linked Lists 
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Node structure containing power and coefficient of variable 
-#include<stdio.h>
-#include<stdlib.h>
+// A structure to represent a stack 
+struct Stack { 
+    int top; 
+    int maxSize;
+    // we are storing string in integer array, this will not give error
+    // as values will be stored in ASCII and returned in ASCII thus, returned as string again
+    int* array; 
+}; 
 
-typedef struct Node 
+struct Stack* create(int max) 
 { 
-	int coeff; 
-	int pow; 
-	struct Node *next; 
-} Node; 
-			
-// Function to create new node 
-void create_node(int x, int y, struct Node **temp) 
-{ 
-	struct Node *r, *z; 
-	z = *temp; 
-	if(z == NULL) 
-	{ 
-		r =(struct Node*)malloc(sizeof(struct Node)); 
-		r->coeff = x; 
-		r->pow = y; 
-		*temp = r; 
-		r->next = (struct Node*)malloc(sizeof(struct Node)); 
-		r = r->next; 
-		r->next = NULL; 
-	} 
-	else
-	{ 
-        printf("Done");
-		r->coeff = x; 
-		r->pow = y; 
-		r->next = (struct Node*)malloc(sizeof(struct Node)); 
-		r = r->next; 
-		r->next = NULL; 
-	} 
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack)); 
+    stack->maxSize = max; 
+    stack->top = -1; 
+    stack->array = (int*)malloc(stack->maxSize * sizeof(int));
+    return stack; 
 } 
 
-// Function Adding two polynomial numbers 
-void polyadd(struct Node *poly1, struct Node *poly2, struct Node *poly) 
+// Checking with this function is stack is full or not
+// Will return true is stack is full else false 
+//Stack is full when top is equal to the last index 
+int isFull(struct Stack* stack) 
 { 
-while(poly1->next && poly2->next) 
-	{ 
-		// If power of 1st polynomial is greater then 2nd, then store 1st as it is 
-		// and move its pointer 
-		if(poly1->pow > poly2->pow) 
-		{ 
-			poly->pow = poly1->pow; 
-			poly->coeff = poly1->coeff; 
-			poly1 = poly1->next; 
-		} 
-		
-		// If power of 2nd polynomial is greater then 1st, then store 2nd as it is 
-		// and move its pointer 
-		else if(poly1->pow < poly2->pow) 
-		{ 
-			poly->pow = poly2->pow; 
-			poly->coeff = poly2->coeff; 
-			poly2 = poly2->next; 
-		} 
-		
-		// If power of both polynomial numbers is same then add their coefficients 
-		else
-		{ 
-			poly->pow = poly1->pow; 
-			poly->coeff = poly1->coeff+poly2->coeff; 
-			poly1 = poly1->next; 
-			poly2 = poly2->next; 
-		} 
-		
-		// Dynamically create new node 
-		poly->next = (struct Node *)malloc(sizeof(struct Node)); 
-		poly = poly->next; 
-		poly->next = NULL; 
-	} 
-while(poly1->next || poly2->next) 
-	{ 
-		if(poly1->next) 
-		{ 
-			poly->pow = poly1->pow; 
-			poly->coeff = poly1->coeff; 
-			poly1 = poly1->next; 
-		} 
-		if(poly2->next) 
-		{ 
-			poly->pow = poly2->pow; 
-			poly->coeff = poly2->coeff; 
-			poly2 = poly2->next; 
-		} 
-		poly->next = (struct Node *)malloc(sizeof(struct Node)); 
-		poly = poly->next; 
-		poly->next = NULL; 
-	} 
-} 
-
-Node * add(Node *poly1, Node *poly2, Node *poly){
-    while (poly1->next && poly2->next)
-    {
-        if(poly1->pow>poly2->pow){
-            poly->pow=poly1->pow;
-            poly->coeff=poly1->coeff;
-            poly1=poly1->next;
-        }else if (poly1->pow<poly2->pow)
-        {
-            poly->pow=poly2->pow;
-            poly->coeff=poly2->coeff;
-            poly2=poly2->next;
-        }else
-        {
-            poly->pow=poly1->pow;
-            poly->coeff=poly1->coeff+poly2->coeff;
-            poly1=poly1->next;
-            poly2=poly2->next;
-        }
-        return poly;
+    if(stack->top == stack->maxSize - 1){
+        printf("Will not be able to push maxSize reached\n");
     }
+    // Since array starts from 0, and maxSize starts from 1
+    return stack->top == stack->maxSize - 1; 
+} 
+
+// By definition the Stack is empty when top is equal to -1 
+// Will return true if top is -1
+int isEmpty(struct Stack* stack) 
+{ 
+    return stack->top == -1; 
+}
+
+// Push function here, inserts value in stack and increments stack top by 1
+void push(struct Stack* stack, int item) 
+{ 
+    if (isFull(stack)) 
+        return; 
+    stack->array[++stack->top] = item; 
+}
+
+// Function to remove an item from stack.  It decreases top by 1 
+int pop(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return INT_MIN; 
+    return stack->array[stack->top--]; 
+} 
+
+// Function to return the top from stack without removing it 
+int peek(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return INT_MIN; 
+    return stack->array[stack->top]; 
+} 
+
+// A utility function to check if the given character is operand 
+int checkIfOperand(char ch) 
+{ 
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'); 
+} 
+
+// Fucntion to compare precedence
+// If we return larger value means higher precedence 
+int precedence(char ch) 
+{ 
+    switch (ch) 
+    { 
+    case '+': 
+    case '-': 
+        return 1; 
+
+    case '*': 
+    case '/': 
+        return 2; 
+
+    case '^': 
+        return 3; 
+    } 
+    return -1; 
+} 
+
+// The driver function for infix to postfix conversion 
+int getPostfix(char* expression) 
+{ 
+    int i, j; 
+
+    // Stack size should be equal to expression size for safety  
+    struct Stack* stack = create(strlen(expression)); 
+    if(!stack) // just checking is stack was created or not  
+        return -1 ; 
+
+    for (i = 0, j = -1; expression[i]; ++i) 
+    { 
+        // Here we are checking is the character we scanned is operand or not
+        // and this adding to to output. 
+        if (checkIfOperand(expression[i])) 
+            expression[++j] = expression[i]; 
+
+        // Here, if we scan character ‘(‘, we need push it to the stack. 
+        else if (expression[i] == '(') 
+            push(stack, expression[i]); 
+
+        // Here, if we scan character is an ‘)’, we need to pop and print from the stack  
+        // do this until an ‘(‘ is encountered in the stack. 
+        else if (expression[i] == ')') 
+        { 
+            while (!isEmpty(stack) && peek(stack) != '(') 
+                expression[++j] = pop(stack); 
+            if (!isEmpty(stack) && peek(stack) != '(') 
+                return -1; // invalid expression              
+            else
+                pop(stack); 
+        } 
+        else // if an opertor
+        { 
+            while (!isEmpty(stack) && precedence(expression[i]) <= precedence(peek(stack))) 
+                expression[++j] = pop(stack); 
+            push(stack, expression[i]); 
+        } 
+
+    } 
+
+    // Once all inital expression characters are traversed
+    // adding all left elements from stack to exp
+    while (!isEmpty(stack)) 
+        expression[++j] = pop(stack); 
+
+    expression[++j] = '\0'; 
     
 }
 
+void reverse(char *exp){
 
-// Display Linked list 
-void show(struct Node *node) 
-{ 
-while(node->next != NULL) 
-	{ 
-	printf("%dx^%d", node->coeff, node->pow); 
-	node = node->next; 
-	if(node->next != NULL) 
-		printf(" + "); 
-	} 
-} 
+    int size = strlen(exp);
+    int j = size, i=0;
+    char temp[size];
 
-// Driver program 
-int main() 
-{ 
-	struct Node *poly1 = NULL, *poly2 = NULL, *poly = NULL; 
-	
-	// Create first list of 5x^2 + 4x^1 + 2x^0 
-	create_node(5,2,&poly1); 
-	create_node(4,1,&poly1); 
-	create_node(2,0,&poly1); 
-	
-	// Create second list of 5x^1 + 5x^0 
-	create_node(5,1,&poly2); 
-	create_node(5,0,&poly2); 
-	
-	printf("1st Number: "); 
-	show(poly1); 
-	
-	printf("\n2nd Number: "); 
-	show(poly2); 
-	
-	poly = (struct Node *)malloc(sizeof(struct Node)); 
-	
-	// Function add two polynomial numbers 
-	add(poly1, poly2, poly); 
-	
-	// Display resultant List 
-	printf("\nAdded polynomial: "); 
-	show(poly); 
+    temp[j--]='\0';
+    while(exp[i]!='\0')
+    {
+        temp[j] = exp[i];
+        j--;
+        i++;
+    }
+    strcpy(exp,temp);
+}
+void brackets(char* exp){
+    int i = 0;
+    while(exp[i]!='\0')
+    {
+        if(exp[i]=='(')
+            exp[i]=')';
+        else if(exp[i]==')')
+            exp[i]='(';
+        i++;
+    }
+}
+void InfixtoPrefix(char *exp){
 
-return 0; 
-} 
+    int size = strlen(exp);
+
+    // reverse string
+    reverse(exp);
+    //change brackets
+    brackets(exp);
+    //get postfix
+    getPostfix(exp);
+    // reverse string again
+    reverse(exp);
+}
+
+int main()
+{    
+    printf("The infix is: ");
+
+    char expression[] = "((a/b)+c)-(d+(e*f))"; 
+    printf("%s\n",expression);
+    InfixtoPrefix(expression); 
+
+    printf("The prefix is: ");
+    printf("%s\n",expression);
+
+    return 0; 
+}
